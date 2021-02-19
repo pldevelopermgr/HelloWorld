8c74faeee59addd3c52408328b72237ac72294f0
mgr0301 <75449341+pldevelopermgr@users.noreply.github.com>
2021-02-19 22:19:56 +0100

    Add files via upload

---
+++ AHelloWorld.java
@@ -0,0 +1,22 @@
+
+//Erstelle eine Klasse mithilfe von: public class Klassenname {}
+
+public class AHelloWorld {
+	
+	//Erschaffe eine Main Methode
+	
+	public static void main(String[] args) {
+		
+		//Gib in der Konsole: Hello World! aus
+		
+		System.out.println("Hello World!");
+		
+		/*
+		 * System = Klasse (Objekt)
+		 * out = Attribut (Output Stream)
+		 * println = Methode von out
+		 */
+		
+	}
+
+}
